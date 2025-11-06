1511ba2a97bcbe26cc27d71506d1b6ec46f45197
Obinna Ejiogu <y5d82@students.keele.ac.uk>
2025-11-06 14:31:07 +0000

    added a score system

--- my_final_assessment.py
+++ my_final_assessment.py
@@ -8,12 +8,24 @@ secret_number = random.randint(1, 10)
 print("Welcome to the Guess the Number Game!")
 print("I'm thinking of a number between 1 and 10.")
 
+# Track number of guesses
+guess_count = 0
+
 # Loop until the user guesses correctly
 while True:
     guess = int(input("Take a guess: "))
+    guess_count += 1
     
     if guess == secret_number:
-        print("🎉 You guessed it! Well done!")
+        print(f"🎉 You guessed it! Well done in {guess_count} tries!")
+        
+        # Feedback based on performance
+        if guess_count == 1:
+            print("🏆 Incredible! You got it on the first try!")
+        elif guess_count <= 3:
+            print("👏 Great job! That was quick.")
+        else:
+            print("👍 Good effort! Practice makes perfect.")
         break
     elif guess < secret_number:
         print("🔼 Too low! Try again.")
