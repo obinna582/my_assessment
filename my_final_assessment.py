9c3d823840baf530c032269886b88f0c7f10955f
Obinna Ejiogu <y5d82@students.keele.ac.uk>
2025-11-06 13:39:01 +0000

    allowed multiple guesses

--- my_final_assessment.py
+++ my_final_assessment.py
@@ -8,11 +8,12 @@ secret_number = random.randint(1, 10)
 print("Welcome to the Guess the Number Game!")
 print("I'm thinking of a number between 1 and 10.")
 
-# Ask the user to guess
-guess = int(input("Take a guess: "))
-
-# Check the guess
-if guess == secret_number:
-    print("🎉 You guessed it! Well done!")
-else:
-    print(f"❌ Nope! The number was {secret_number}. Better luck next time!")
+# Loop until the user guesses correctly
+while True:
+    guess = int(input("Take a guess: "))
+    
+    if guess == secret_number:
+        print("🎉 You guessed it! Well done!")
+        break
+    else:
+        print("❌ Nope! Try again.")
